80d61aef28b7bb631d3f6c6efc9b90878897a993
Juhász István <istvan.juhasz@telcotrend.hu>
2021-06-10 16:47:26 +0200

    create auth module for vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
 import { createStore } from 'vuex'
+import { auth } from "./auth.module";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+
+  modules: {auth
   }
 })
