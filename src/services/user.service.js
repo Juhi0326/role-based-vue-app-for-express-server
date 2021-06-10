ebd11a7a554c23545e8d4f63339e7430d1b3ad36
Juhász István <istvan.juhasz@telcotrend.hu>
2021-06-10 16:44:01 +0200

    create user.service.js file

---
+++ src/services/user.service.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'http://localhost:8080/';
+
+class UserService {
+  getHomePage() {
+    return axios.get(API_URL + 'home');
+  }
+
+  getProducts() {
+    return axios.get(API_URL + 'products', { headers: authHeader() });
+  }
+
+  getOrders() {
+    return axios.get(API_URL + 'orders', { headers: authHeader() });
+  }
+
+  getImages() {
+    return axios.get(API_URL + 'image-setup', { headers: authHeader() });
+  }
+}
+
+export default new UserService();
