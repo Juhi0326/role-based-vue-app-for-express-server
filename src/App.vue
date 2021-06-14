b2f397225f4fb4ebe15b4e03d30dffaee2060a9c
Juhász István <istvan.juhasz@telcotrend.hu>
2021-06-14 16:42:20 +0200

    create bootstrap navbar for routerlinks

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/Products">Products</router-link> |
-    <router-link to="/Login">Login</router-link> |
-  </div>
-  <router-view/>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link to="/" class="nav-link">Home</router-link>
+          <router-link to="/Products" class="nav-link">Products</router-link> 
+          <router-link to="/Login" class="nav-link">Login</router-link> 
+        </div>
+      </div> 
+    </div>
+  </nav>
+
+  <router-view />
 </template>
 
 <style>
